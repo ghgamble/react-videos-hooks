6e25bfa9093f6b06b3ce50daf7ca9e3bf0870433
gracehg <grace@statusforward.com>
2020-11-10 10:53:24 -0500

    add custom hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,19 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-import youtube from '../apis/youtube';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-      const [videos, setVideos] = useState([]);
       const [selectedVideo, setSelectedVideo] = useState(null);
+      const [videos, search] = useVideos('dogs');
 
-      useEffect(() => {
-            onTermSubmit('dogs');
-      }, []);
-
-      const onTermSubmit = async term => {
-            const response = await youtube.get('/search', {
-                  params: {
-                        q: term
-                  }
-            })
-            setVideos(response.data.items);
-            setSelectedVideo(response.data.items[0]);
-      }
+      useEffect(() =>  {
+            setSelectedVideo(videos[0]);
+      }, [videos]);
 
       return (
             <div className="ui container" style={{ marginTop: "10px" }}>
-                  <SearchBar onFormSubmit={onTermSubmit} />
+                  <SearchBar onFormSubmit={search} />
                   <div className="ui grid">
                         <div className="ui row">
                               <div className="eleven wide column">
